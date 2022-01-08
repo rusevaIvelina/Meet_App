d613a0028d4854e33eb1b1ee585f52501507bf6c
rusevaIvelina <iva.ruseva14@gmail.com>
2022-01-07 19:23:08 -0500

    imported CitySearch component

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,16 +2,20 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
 import EventList from '../EventList';
+import CitySearch from '../CitySearch';
 
 describe('<App /> component', () => {
 
+    let AppWrapper;
+    beforeAll(()=> {
+        AppWrapper = shallow(<App/>)
+    });
+
     test('render list of events', () => {
-        const AppWrapper = shallow(<App />);
-        expect(AppWrapper.find(EventList)).toHaveLength(1);
+        expect(AppWrapper.find(EventList)).toHaveLength(1); // const AppWrapper = shallow(<App />)
     });
 
     test('render CitySearch', () => {
-        const AppWrapper = shallow(<App/>);
         expect(AppWrapper.find(CitySearch)).toHaveLength(1);
     });
 
