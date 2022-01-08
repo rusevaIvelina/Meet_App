a80661e6d171d9c77b03dff2dbd90eef5fabec87
rusevaIvelina <iva.ruseva14@gmail.com>
2022-01-07 19:38:14 -0500

    added a test for suggestions

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -3,8 +3,13 @@ import { shallow } from 'enzyme';
 import CitySearch from '../CitySearch';
 
 describe('<CitySearch /> component', () => {
+
   test('render text input', () => {
     const CitySearchWrapper = shallow(<CitySearch />);
     expect(CitySearchWrapper.find('.city')).toHaveLength(1);
   });
+  test('render a list of suggestions', () => {
+      const CitySearchWrapper = shallow(<CitySearch/>);
+      expect(CitySearchWrapper.find('.suggestions')).toHaveLength(1);
+  })
 });
