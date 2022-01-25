288158b4a842ee48db8d6c5c591fa16d5d0a1d32
rusevaIvelina <iva.ruseva14@gmail.com>
2022-01-25 11:41:40 -0500

    added Number of events

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
+import NumberOfEvents from './NumberOfEvents';
 
 class App extends Component {
   state = {
     events: [],
-    locations: []
+    locations: [],
+    numberOfEvents: 32,
   }
   render() {
     return (
       <div className="App">
         <CitySearch locations={this.state.locations}/>
         <EventList events={this.state.events}/>
+        <NumberOfEvents numberOfEvents={this.state.numberOfEvents}/>
       </div>
     );
   }
---
+++ src/NumberOfEvents.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export class NumberOfEvents extends Component {
+
+    
+    render() {
+        return(
+        <div className="NumberOfEvents">
+            <input type="number" className="number"
+            min="1" max="100"
+            value={this.props.numberOfEvents} 
+            onChange={(e) => this.props.updateNumberOfEvents(e)}/>
+        </div>
+        
+
+        )
+    }};
+
+
+export default NumberOfEvents;
