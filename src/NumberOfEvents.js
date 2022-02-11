f71426847366312ed6c68ac6567cae43df412dc4
rusevaIvelina <iva.ruseva14@gmail.com>
2022-02-11 12:54:12 -0500

    Update NumberOfEvents.js

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -9,7 +9,7 @@ import { ErrorAlert } from './Alert';
             <ErrorAlert text={this.props.errorText}/>
             <label className='number-label'> Number of Events </label>
             <input type="number" className="number"
-            min="1" max="32"
+            min="1" max="50"
             value={this.props.numberOfEvents} 
             onChange={(e) => this.props.updateNumberOfEvents(e)}/>
         </div>
