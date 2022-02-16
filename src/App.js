f270d179a260bf9082da32a4a709b53ddf44e305
rusevaIvelina <iva.ruseva14@gmail.com>
2022-02-16 17:15:19 -0500

    added a scatterchart

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import NumberOfEvents from './NumberOfEvents';
 import { extractLocations, getEvents, checkToken, getAccessToken } from './api';
 import { WarningAlert } from './Alert';
 import WelcomeScreen from './WelcomeScreen';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 
 class App extends Component {
   state = {
@@ -109,6 +110,22 @@ class App extends Component {
           <NumberOfEvents numberOfEvents={this.state.numberOfEvents} 
           updateNumberOfEvents={this.updateNumberOfEvents}
           errorText = {this.state.errorText}/>
+          <h4>Events in each city</h4>
+
+          <ScatterChart
+          width={400}
+          height={400}
+          margin={{
+            top: 20, right: 20, bottom: 20, left: 20,
+          }}
+        >
+          <CartesianGrid />
+          <XAxis type="number" dataKey="x" name="stature" unit="cm" />
+          <YAxis type="number" dataKey="y" name="weight" unit="kg" />
+          <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+          <Scatter  data={this.getData()} fill="#8884d8" />
+        </ScatterChart>
+
         <EventList events={this.state.events}/>
       </div>
     )};
@@ -117,6 +134,7 @@ class App extends Component {
         <div className='App'>
           <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={() => { getAccessToken()}} />
         </div>
+        
       )
     }
   }
